30dd8a4a467d54beb289e0f32d8dbaf52e48ef50
Alvaro297 <alvaromartinfalagan29@gmail.com>
2025-10-12 19:40:30 +0200

    Solucion socket

--- src/server/Server.cpp
+++ src/server/Server.cpp
@@ -15,11 +15,15 @@ void Server::initSockets()
 	for (size_t i = 0; i < this->_servers.size(); i++)
 	{
 		fullhost = _servers[i].getHost() + ":" + std::to_string(_servers[i].getPort());
-		if (fdSocket = socket(AF_INET, SOCK_STREAM, 0) < 0)
+		if (this->_listeners.find(fullhost) == this->_listeners.end())
+			fdSocket = socket(AF_INET, SOCK_STREAM, 0);
+		if (fdSocket < 0)
 		{
 			std::cerr << "socket() failed for " << fullhost << ": " << strerror(errno) << std::endl;
 			continue;
 		}
-		this->_listeners[fullhost] = fdSocket;
+		if (this->_listeners.find(fullhost) == this->_listeners.end())
+			this->_listeners[fullhost] = fdSocket;
+		
 	}
 }
